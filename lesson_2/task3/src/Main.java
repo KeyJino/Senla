2a602a253364fab340ed3b97e4e1d8bdfa715e78
KeyJino <jeka.kova@mail.ru>
2017-10-30 01:49:03 +0300

    reload lesson

---
+++ lesson_2/task3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.assembleProduct(new InstalationProducts());
+        System.out.println();
+
+    }
+}
---
+++ lesson_2/task3/src/ProductEngine.java
@@ -0,0 +1,5 @@
+
+public class ProductEngine implements IProductPart{
+    String productEngine;
+    ProductEngine(String productEngine){this.productEngine = productEngine;}
+}
