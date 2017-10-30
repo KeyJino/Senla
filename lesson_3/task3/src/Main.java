4a51f5fa610d6a5112cba72c40d000b4f97ec8df
KeyJino <jeka.kova@mail.ru>
2017-10-30 22:43:34 +0300

    fix project logic

--- lesson_3/task3/src/Main.java
+++ lesson_3/task3/src/Main.java
@@ -1,7 +1,21 @@
 
 public class Main {
     public static void main(String[] args) {
-        Stock stock = new Stock(10000);
-        stock.addProduct();
+
+        Fridge fridge = new Fridge("Fridge", 140);
+        TV tv = new TV("TV", 30);
+        Kettles kettles = new Kettles("Kettles", 1);
+        Washer washer = new Washer("Washer", 40);
+        MicrowaveOven microwaveOven = new MicrowaveOven("Microwave Oven", 7);
+
+        Stock stock = new Stock(2000);
+
+        while (stock.isFull()){
+            stock.addProduct(fridge);
+            stock.addProduct(tv);
+            stock.addProduct(kettles);
+            stock.addProduct(washer);
+            stock.addProduct(microwaveOven);
+        }
     }
 }
--- lesson_3/task3/src/Stock.java
+++ lesson_3/task3/src/Stock.java
@@ -1,54 +1,46 @@
 
 public class Stock{
 
-    private boolean flag = false;
     private int maxWeight;
     private int weigth = 0;
+    private boolean flag = true;
     private Product[] stock = new Product[1];
 
-    Fridge fridge = new Fridge("Fridge", 140);
-    TV tv = new TV("TV", 30);
-    Kettles kettles = new Kettles("Kettles", 1);
-    Washer washer = new Washer("Washer", 40);
-    MicrowaveOven microwaveOven = new MicrowaveOven("Microwave Oven", 7);
 
-    Product[] products = {fridge, tv, kettles, washer, microwaveOven};
 
     Stock(int maxWeight){
         this.maxWeight = maxWeight;
     }
 
-    public void addProduct(){
+    public void addProduct(Product product){
+                    if(isDirty(product)) {
+                        setWeght(product);
+                        // Adding products to the stock.
+                        stock = product.addProduct(stock, product);
 
-        while ((getWeight() <= maxWeight) != flag) {
-            for (int i = 0; i < products.length; i++) {
-                if (weigth + products[i].getWeight() <= maxWeight) {
+                        // Calculation of the weight.
 
-                    // Adding products to the stock.
-                    stock = products[i].addProduct(stock, products[i]);
-
-                    // Calculation of the weight.
-                    weigth += products[i].getWeight();
-
-                    System.out.println(products[i].getName() + " - " + weigth);
-                    System.out.println();
-                } else {
-                    continue;
-                }
-
-            }
-            // Until the stock is full
-            if((getWeight() + 1 <= maxWeight) == false)
-                flag = true;
-        }
-
+                        System.out.println(product.getName() + " - " + weigth);
+                        System.out.println();
+                    }
+                    if(getStockWeight() == maxWeight){
+                        flag = false;
+                    }
     }
 
+    private void setWeght(Product product){
+        weigth += product.getWeight();
+    }
 
-
-
-
-    public int getWeight(){
+    public int getStockWeight(){
         return  weigth;
     }
+
+    public boolean isFull(){
+        return flag;
+    }
+    public boolean isDirty(Product product){
+        return getStockWeight() + product.getWeight() <= maxWeight;
+    }
+
 }
